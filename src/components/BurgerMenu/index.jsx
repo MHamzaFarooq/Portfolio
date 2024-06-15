f7a471b3205caaf6f0ab234463f7c87dd8e75ba7
M-Ali-Haider <mah01134212100@gmail.com>
2024-06-15 22:58:38 +0500

    added burger menu animation

--- src/components/BurgerMenu/index.jsx
+++ src/components/BurgerMenu/index.jsx
@@ -1,7 +1,41 @@
+import { useLayoutEffect, useRef } from "react";
 import styles from "./style.module.css";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 export default function BurgerMenu({ setIsSidebarOpen }) {
+  const burgerRef = useRef(null);
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(burgerRef.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: 55,
+        onLeave: () => {
+          gsap.to(burgerRef.current, {
+            scale: 0,
+            duration: 0.5,
+            ease: "poweri.out",
+          });
+        },
+        onEnterBack: () => {
+          gsap.to(burgerRef.current, {
+            scale: 1,
+            duration: 0.5,
+            ease: "poweri.out",
+          });
+        },
+      },
+    });
+  }, []);
+
   return (
-    <div className={styles.burgerMenu} onClick={() => setIsSidebarOpen(true)}>
+    <div
+      ref={burgerRef}
+      className={styles.burgerMenu}
+      onClick={() => setIsSidebarOpen(true)}
+    >
       <div className={styles.bmLine}></div>
       <div className={styles.bmLine}></div>
       <div className={styles.bmLine}></div>
